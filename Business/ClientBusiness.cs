9844041dfefcb00f4787309d4c5846530ac9e23f
Kolyo1 <dimovnikola705@gmail.com>
2026-03-27 09:16:10 +0200

    dobavih malko view moldel methodi

--- Business/ClientBusiness.cs
+++ Business/ClientBusiness.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Shared.ViewModels;
 
 namespace Business
 {
@@ -8,5 +9,27 @@ namespace Business
     {
         public ClientBusiness(ExpressDbContext context) : base(context) { }
         public ClientBusiness() : base() { }
+        public async Task<ClientViewModel> GetClientViewById(int id)
+        {
+            var client = await GetById(id);
+            return MapToViewModel(client);
+        }
+
+        public async Task<List<ClientViewModel>> GetAllShipmentViews()
+        {
+            var clients = await GetAll();
+            return clients.Select(MapToViewModel).ToList();
+        }
+        private ClientViewModel MapToViewModel(Client s)
+        {
+            return new ClientViewModel
+            {
+                Id = s.Id,
+                FullName = $"{s.FirstName} {s.LastName}",
+                Phone = $"{s.Phone}",
+                Email = $"{s.Email}",
+                Address = $"{s.Address}",
+            };
+        }
     }
 }
